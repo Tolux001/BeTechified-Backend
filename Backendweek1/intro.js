db47393a88a86e484aee33c5c966d19be02b71e9
Tolux001 <toluwalasepeter100@gmail.com>
2026-02-14 15:23:19 +0100

    add: week 1 assignment

---
+++ Backendweek1/intro.js
@@ -0,0 +1,8 @@
+// Backend development Week one(1)
+const studentName = "Toluwalase Peter";
+const age = 22;
+const fav_lang = "JavaScript";
+
+console.log(`My name is ${studentName}`);
+console.log(`I am ${age} years old`);
+console.log(`My favorite programming language is ${fav_lang}`);
